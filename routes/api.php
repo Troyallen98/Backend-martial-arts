b41d4ac87b3537166104be070b4c166bb652d3f1
troy.wagonera <troy.wagonera@gmail.com>
2021-11-15 13:50:38 -0500

    update qucik quick

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', function(Request $request){
     return 'Authenticated';
 });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+
+});
 
